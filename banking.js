a8fd641b4a18ace1ece75dbf0b5ae2d8b9a6e3c3
Mst Rinky Akter <mstrinkya@gmail.com>
2022-10-19 23:32:38 +0600

    finis

--- banking.js
+++ banking.js
@@ -1,7 +1,69 @@
 document.getElementById('deposit-amount').addEventListener('click',function(){
   const depositInput=document.getElementById('deposit-input');
-  const depositAmount=depositInput.value ;
+
+ const newDepositAmountText=depositInput.value;
+
+ const newDepositAmount=parseFloat(newDepositAmountText);
+
  const depositTotal=document.getElementById('deposit-total');
- depositTotal.innerText=depositAmount;
- depositInput.value='';
+
+ const previousDepositText=depositTotal.innerText;
+
+ const previousDepositAmount=parseFloat(previousDepositText);
+
+  const newDepositTotal=previousDepositAmount+newDepositAmount;
+
+depositTotal.innerText=newDepositTotal;
+
+
+
+
+ const balanceTotal=document.getElementById('balance-total');
+
+const balanceTotalText=balanceTotal.innerText;
+
+const previousBalanceTotal=parseFloat(balanceTotalText)
+
+const newBalanceTotal=previousBalanceTotal+newDepositAmount;
+
+balanceTotal.innerText=newBalanceTotal;
+
+depositInput.value='';
+
 })
+
+
+// handle withdraw event handle 
+document.getElementById('withdraw-amount').addEventListener('click',function(){
+  // console.log('withdraw clicked')
+   
+  const withdrawInput=document.getElementById('withdraw-input');
+  const withdrawAmountText=withdrawInput.value;
+  const newWithdrawAmount=parseFloat(withdrawAmountText);
+  console.log(newWithdrawAmount);
+
+  // set withdraw total 
+
+  const withdrawTotal=document.getElementById('withdraw-total');
+
+  const previousWithdrawText=withdrawTotal.innerText;
+
+  const previousWithdrawTotal=parseFloat(previousWithdrawText);
+
+  const newWithdrawTotal=previousWithdrawTotal + newWithdrawAmount;
+  withdrawTotal.innerText=newWithdrawTotal;
+   
+  // update balance 
+  const balanceTotal=document.getElementById('balance-total');
+  const previousBalanceText=balanceTotal.innerText;
+  const previousBalanceTotal=parseFloat(previousBalanceText);
+  const newBalanceTotal=previousBalanceTotal - newWithdrawAmount;
+  balanceTotal.innerText=newBalanceTotal;
+
+
+// dlear withdraw input 
+withdrawInput.value='';
+
+})
+
+
